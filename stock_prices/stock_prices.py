3ae60f32412aae75e825a295a4365c19b8481a69
david-pok <david.pok@gmail.com>
2019-10-09 14:34:14 -0700

    stock_prices has a bug

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,9 +3,19 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  #given a list, find smallest element, and the biggest element
+  # find the difference ONLY IF the smaller element comes BEFORE the largest element
+  #return difference
+  differences = []
+  for i in range(len(prices)-1,0,-1):
+    for j in range(i,0,-1):
+      if prices[j] < prices[i]:
+        differences.append(prices[i] - prices[j])
+  print("DIFF", differences)
+  return max(differences)
 
-
+#[10, 7, 5, 8, 11, 9] 6
+#[1050, 270, 1540, 3800, 2] 3530
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
